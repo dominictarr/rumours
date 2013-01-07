3eb64408b1e7cf57620904e39b9cbb1d291facbb
Dominic Tarr <dominic.tarr@gmail.com>
2013-01-07 21:57:37 +0700

    tidy

--- example/client.js
+++ example/client.js
@@ -1,12 +1,13 @@
 
 var rumours = require('../client')
 
-rumours({username: 'dominic', name: 'hello'})
+rumours({username: 'dominic', name: 'hello', host: 'http://localhost:4567'})
   .on('client', function (client) {
     console.log('CLIENT', client)
     client.open('r-edit_text', function (err, edit) {
       if(err) {console.log(err); throw err}
       console.log('EDIT')
-      edit.wrap(document.getElementById('text'))
+      console.log(document.getElementById('ta'))
+      edit.wrap(document.getElementById('ta'))
     })
   })
