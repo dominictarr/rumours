c41e606610fef326da0e47a5062fadd7e24139cc
Dominic Tarr <dominic.tarr@gmail.com>
2013-01-11 23:24:40 +0700

    update example to new api

--- example/client.js
+++ example/client.js
@@ -1,13 +1,19 @@
 
 var rumours = require('../client')
 
-rumours({username: 'dominic', name: 'hello', host: 'http://localhost:4567'})
-  .on('client', function (client) {
-    console.log('CLIENT', client)
-    client.open('r-edit_text', function (err, edit) {
-      if(err) {console.log(err); throw err}
-      console.log('EDIT')
-      console.log(document.getElementById('ta'))
-      edit.wrap(document.getElementById('ta'))
-    })
-  })
+//okay, so next is to make scuttlebutt-remote buffer until it's connected,
+//and move buffering to that part - out of level-scuttlebutt.
+
+//port wikiwiki to just use rumours, and more demos.
+//a better renderer for r-array,
+//and a chat widget.
+
+var client = rumours({username: 'dominic', host: 'http://localhost:4567'}).remote
+
+client.open('r-edit_text', function (err, edit) {
+  if(err) {console.log(err); throw err}
+  console.log('EDIT')
+  console.log(document.getElementById('ta'))
+  edit.wrap(document.getElementById('ta'))
+})
+
---
+++ example/run.sh
@@ -0,0 +1,1 @@
+browserify client.js -o static/bundle.js  --debug && node ../server.js --port 4567
