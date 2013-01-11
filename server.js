9277a645bd4350385b02cc7cd833a41f30ecbdc8
Dominic Tarr <dominic.tarr@gmail.com>
2013-01-11 23:48:45 +0700

    easier to use in your program

--- server.js
+++ server.js
@@ -12,39 +12,37 @@ var through  = require('through')
 var fs = require('fs')
 
 module.exports = function (config) {
+  config = config || {}
   var udid         = require('udid')(config.name || 'rumours')
-  var loadDb       = require('./db')()
+  var loadDb       = require('./db')(config)
+
+  var server =  http.createServer(ecstatic(config.static))
+
 
   shoe(reloader(function (stream) {
     console.log('connection')
     var ts = through().pause()
     
-//    stream.pipe(ts)
-//    stream.on('data', console.log)
-
     var dbName = stream.meta.db
     var db = loadDb(dbName, function (err, db) {
-      console.log('LOADED DB', db.remote)
       if(err) {
         console.error(err)
         stream.end()
         ts.resume()
         return
       }
-
-      //ts.pipe(db.remote.createStream()).pipe(stream)
-      //ts.resume()
     })
 
     stream.pipe(db.remote.createStream()).pipe(stream)
 
-  })).install(
-    http.createServer(ecstatic(config.static))
-     .listen(config.port, function () {
-      console.log( 'listening on', config.port)
-    })
-  , config.prefix || '/' + (config.name || 'rumours'))
+  })).install(server, config.prefix || '/' + (config.name || 'rumours'))
+
+  return server
 }
 
 if(!module.parent)
   module.exports(require('./config'))
+    .listen(config.port, function () {
+      console.log( 'listening on', config.port)
+    })
+
