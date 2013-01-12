48b5135248458ac92f67c943173cfa88936327c2
Dominic Tarr <dominic.tarr@gmail.com>
2013-01-12 09:30:32 +0700

    add @Raynos's scuttlebutts to the default schema

--- schema.js
+++ schema.js
@@ -12,9 +12,8 @@ module.exports = require('scuttlebutt-schema')({
   'r-edit': create(require('r-edit')),
   'r-array': create(require('r-array')),
   'crdt': create(require('crdt'))
-  //,
-  //can't use this until @Raynos updates them to scuttlebutt ~5.5
-  //'append-only': create(require('append-only')),
-  //'expiry-model': create(require('expiry-model'))
+  'append-only': create(require('append-only')),
+  'expiry-model': create(require('expiry-model'))
+  //hmm, what about scuttlebucket?
 })
 
