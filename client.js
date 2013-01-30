fcda13b84e24045152d386170d0d9b98254da97a
Dominic Tarr <dominic.tarr@gmail.com>
2013-01-30 21:11:30 +1300

    remember connections (for live editing)

--- client.js
+++ client.js
@@ -1,10 +1,10 @@
 var reconnect = require('reconnect')
 var reloader  = require('client-reloader')
-
+var shasum    = require('shasum')
 var Remote    = require('level-scuttlebutt/client')
 
-//putting this inside of page is wrong actually...
-//the pushState and the stream are orthagonal.
+//memoize the connections so that this works nicely inside http://github.com/dominictarr/demonstrate
+var connections = {}
 
 module.exports = function (config) {
   config = config || {}
@@ -14,6 +14,10 @@ module.exports = function (config) {
   //but later, it will be rumours.nearform.com or whatever...
 
   var host = config.host || window.location.protocol + '//' + window.location.host
+
+  var tmpId = shasum(JSON.stringify([udid, config, host]))
+
+  if(connections[tmpId]) return connections[tmpId]
 
   config.name = config.name || 'rumours'
   config.prefix = config.prefix || '/rumours'
@@ -25,6 +29,8 @@ module.exports = function (config) {
     //should do this a different way, to avoid that when using custom schemas.
     stream.pipe(remote.createRemoteStream()).pipe(stream)
   }, config)).connect(host + config.prefix)
+
+  connections[tmpId] = r
 
   r.open = remote.open
   r.view = remote.view
