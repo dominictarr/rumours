401b846b33b66d11da972abf73367117626c48db
Dominic Tarr <dominic.tarr@gmail.com>
2013-02-28 20:08:05 +1300

    add r-value

--- schema.js
+++ schema.js
@@ -7,13 +7,14 @@ function create (con) {
 //all the scuttlebutt subclasses written so far.
 
 module.exports = {
-  'model': create(require('scuttlebutt/model')),
-  'events': create(require('scuttlebutt/events')),
-  'r-edit': create(require('r-edit')),
-  'r-array': create(require('r-array')),
-  'crdt': create(require('crdt')),
-  'append-only': create(require('append-only')),
-  'expiry-model': create(require('expiry-model'))
+  'model'       : create(require('scuttlebutt/model')),
+  'events'      : create(require('scuttlebutt/events')),
+  'r-edit'      : create(require('r-edit')),
+  'r-array'     : create(require('r-array')),
+  'crdt'        : create(require('crdt')),
+  'append-only' : create(require('append-only')),
+  'expiry-model': create(require('expiry-model')),
+  'value'       : create(require('r-value'))
   //hmm, what about scuttlebucket?
 }
 
